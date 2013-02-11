8db11f81b36b81a3206f9949dd5d52c4bef8bb82
Jia Guo <guojia@gmail.com>
2013-02-11 14:56:15 +0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wormsay::Application.config.secret_token = '883ce9a3d2fc163b9169355d554ec63d843c9d5b20d9ea07a7e624743cd91c3244e12349965dc2cf2f21023a56034e353c764ce8e6be3d2dae7b766b8bad7342'
